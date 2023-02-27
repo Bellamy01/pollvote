b602378818d3b2ad2438a271276c0f29f210c513
Bellamy <bdanbellamy@gmail.com>
2023-02-27 22:23:48 +0200

    Playing with the API

--- polls/models.py
+++ polls/models.py
@@ -7,8 +7,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('datepublished')
-    def __str__(self) -> str:
-        return self.__str__()
+    def __str__(self):
+        return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -16,3 +16,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
