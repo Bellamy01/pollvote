c100f935f61c59280cbbbde86a8b90383ee9c3f2
Bellamy <bdanbellamy@gmail.com>
2023-03-01 08:25:29 +0200

    Creating the result view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse,Http404
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -11,8 +13,20 @@ def detail(request, question_id):
     return render(request,'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at results of %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html',{ 'question': question })
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." %question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes+= 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
